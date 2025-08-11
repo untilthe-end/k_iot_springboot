2a3615fe10df2abb2b8987f8a5973e38d47a82fa
untilthe-end <letsgohangun@gmail.com>
2025-08-11 09:49:19 +0900

    springboot

---
+++ src/main/java/com/example/k5_iot_springboot/K5IotSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.k5_iot_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K5IotSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K5IotSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/k5_iot_springboot/K5IotSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k5_iot_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K5IotSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
