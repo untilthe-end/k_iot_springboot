ed94785bb5ab028d8a0b68c7c23c0e777757d161
untilthe-end <letsgohangun@gmail.com>
2025-10-01 16:14:34 +0900

    fix/UserPrincipalMapper

--- src/main/java/com/example/k5_iot_springboot/security/UserPrincipalMapper.java
+++ src/main/java/com/example/k5_iot_springboot/security/UserPrincipalMapper.java
@@ -48,7 +48,7 @@ public class UserPrincipalMapper {
                         ? List.of(new SimpleGrantedAuthority("ROLE_USER"))
                         : user.getUserRoles().stream()
                         .map(r -> {
-                            String name = r.getRole().toString();
+                            String name = r.getRole().getName().name();
                             String role = name.startsWith("ROLE_") ? name : "ROLE_" + name;
                             return new SimpleGrantedAuthority(role);
                         })
